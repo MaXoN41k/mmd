029b331aa115cbff78b52e2d29f53070a5756a4b
MaXoN41k <90177831+MaXoN41k@users.noreply.github.com>
2024-03-01 15:03:49 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,69 +1,118 @@
 #include <iostream>
-#include <time.h>
-#include <stdlib.h>
-#include <math.h>
-using namespace std;
+#include <vector>
+
+double Trapecion(std::vector<double> Napruga1, double h)
+{
+  int n = Napruga1.size();
+  double step = (h / (n-1));
+  double res = (step / 2) * (Napruga1[0] + Napruga1[n - 1]);
+  for (int count = 1; count < n-1; ++count)
+  {
+    res += step * Napruga1[count];
+  }
+  return res;
+}
+
+double Simpson(std::vector<double> Napruga1, double h)
+{
+  int n = Napruga1.size();
+  double step = (h / (n - 1));
+  double res =(Napruga1[0] + Napruga1[n - 1]);
+  for (int count = 1; count < n - 1; ++count)
+  {
+    if (count % 2 == 1)
+    {
+      res += 4 * Napruga1[count];
+    }
+    else if (count % 2 == 0)
+    {
+      res += 2 * Napruga1[count];
+    }
+  }
+  return res*step/3;
+}
+
+//lin
+std::vector<double> NaprugaLin(double height, int n)
+{
+  std::vector<double> mas(n);
+  for (int count = 0; count < n; ++count)
+  {
+    mas[count] = -(height/2) + count*(height/(n-1));
+    std::cout << "«начение в узле " << count << ":" << mas[count] << std::endl;
+  }
+  return mas;
+}
+
+//cube
+std::vector<double> NaprugaCube(double height, int n)
+{
+  std::vector<double> mas(n);
+  for (int count = 0; count < n; ++count)
+  {
+    mas[count] = (-(height / 2) + count * ((height / (n - 1)))) * (-(height / 2) + count * ((height / (n - 1)))) * (-(height / 2) + count * ((height / (n - 1))));
+    std::cout << "«начение в узле " << count << ":" << mas[count] << std::endl;
+  }
+  return mas;
+}
+
+//const
+std::vector<double> NaprugaConst(double height, int n)
+{
+  std::vector<double> mas(n);
+  for (int count = 0; count < n; ++count)
+  {
+    mas[count] = 5;
+    std::cout << "«начение в узле " << count << ": " << mas[count] << std::endl;
+  }
+  return mas;
+}
+
+//prarab
+std::vector<double> NaprugaParab(double height, int n)
+{
+  std::vector<double> mas(n);
+  for (int count = 0; count < n; ++count)
+  {
+    mas[count] = (-(height / 2) + count * ((height / (n - 1))))*(-(height / 2) + count * ((height / (n - 1))));
+    std::cout << "«начение в узле " << count <<":"<< mas[count] << std::endl;
+  }
+  return mas;
+}
+
+//kvadro
+std::vector<double> NaprugaQadr(double height, int n)
+{
+  std::vector<double> mas(n);
+  for (int count = 0; count < n; ++count)
+  {
+    mas[count] = (-(height / 2) + count * ((height / (n - 1)))) * (-(height / 2) + count * ((height / (n - 1)))) * (-(height / 2) + count * ((height / (n - 1)))) * (-(height / 2) + count * ((height / (n - 1))));
+    std::cout << "«начение в узле " << count << ":" << mas[count] << std::endl;
+  }
+  return mas;
+}
 
 int main()
 {
-  srand(time(0));
-  double lambda;
-  int count;
+  setlocale(LC_ALL,"rus");
+  int yzel=0;
+  int h;
 
-  cout << "Vvedite lambda ";
-  cin >> lambda;
-  cout << endl;
-
-  cout << "Vvedite chislo opitov ";
-  cin >> count;
-  cout << endl;
-
-  double x = 0;
-  double y = 0;
-  
-  /*
-  double max = 0;
-  for (int c = 0; c < 1000; ++c)
+  while (yzel % 2 != 1)
   {
-    x = ((double)rand()) / RAND_MAX;
-    if (x < 0.5)
-    {
-      y = (log(2 * x)) / (lambda);
-    }
-    else if (x > 0.5)
-    {
-      y = -((log(2 - (2 * x))) / (lambda));
-    }
-    else
-    {
-      y = 0;
-    }
-    if (y > max)
-    {
-      max = y;
-    }
-
-    cout << y <<endl;
-  }
-  */
-  for (int n = 0; n < count; n++)
-  {
-    x = ((double)rand()) / RAND_MAX;
-    if (x < 0.5)
-    {
-      y = (log(2 * x)) / (lambda);
-    }
-    else if (x > 0.5)
-    {
-      y = -((log(2 - (2 * x))) / (lambda));
-    }
-    else if (x = 0.5)
-    {
-      y = 0;
-    }
-
-    cout << y <<endl;
+    std::cout << "¬ведите количество узлов (нечЄтное)" << std::endl;
+    std::cin >> yzel;
+    std::cout << std::endl;
   }
 
+  std::cout << "¬ведите высоту" << std::endl;
+  std::cin >> h;
+  std::cout << std::endl;
+
+  std::vector<double> nap = NaprugaCube(h, yzel + 2);
+
+  //double res = Trapecion(nap, h);
+  double res = Simpson(nap, h);
+  std::cout << res;
   return 0;
 }
