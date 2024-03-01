fae6cad9953b8c589f1b913838dffa73078fb0b7
MaXoN41k <90177831+MaXoN41k@users.noreply.github.com>
2024-03-01 15:02:58 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,44 +1,69 @@
 #include <iostream>
-#include <vector>
-
-double Trapecion(std::vector<double> Napruga1, double h)
-{
-  int n = Napruga1.size();
-  double step = (h / (n-1));
-  double res = (step / 2) * (Napruga1[0] + Napruga1[n - 1]);
-  for (int count = 1; count < n-1; ++count)
-  {
-    res += step * Napruga1[count];
-  }
-  return res;
-}
-
-std::vector<double> Napruga(double height, int n)
-{
-  std::vector<double> mas(n);
-  for (int count = 0; count < n; ++count)
-  {
-    mas[count] = -(height/2) + count*(height/(n-1));
-  }
-  return mas;
-}
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+using namespace std;
 
 int main()
 {
-  int yzel=1;
-  int h;
+  srand(time(0));
+  double lambda;
+  int count;
 
-  std::cout << "¬ведите количество узлов" << std::endl;
-  std::cin >> yzel;
-  std::cout << std::endl;
+  cout << "Vvedite lambda ";
+  cin >> lambda;
+  cout << endl;
 
-  std::cout << "¬ведите высоту" << std::endl;
-  std::cin >> h;
-  std::cout << std::endl;
+  cout << "Vvedite chislo opitov ";
+  cin >> count;
+  cout << endl;
 
-  std::vector<double> nap = Napruga(h, yzel + 2);
+  double x = 0;
+  double y = 0;
+  
+  /*
+  double max = 0;
+  for (int c = 0; c < 1000; ++c)
+  {
+    x = ((double)rand()) / RAND_MAX;
+    if (x < 0.5)
+    {
+      y = (log(2 * x)) / (lambda);
+    }
+    else if (x > 0.5)
+    {
+      y = -((log(2 - (2 * x))) / (lambda));
+    }
+    else
+    {
+      y = 0;
+    }
+    if (y > max)
+    {
+      max = y;
+    }
 
-  double res = Trapecion(nap,h);
-  std::cout << res;
+    cout << y <<endl;
+  }
+  */
+  for (int n = 0; n < count; n++)
+  {
+    x = ((double)rand()) / RAND_MAX;
+    if (x < 0.5)
+    {
+      y = (log(2 * x)) / (lambda);
+    }
+    else if (x > 0.5)
+    {
+      y = -((log(2 - (2 * x))) / (lambda));
+    }
+    else if (x = 0.5)
+    {
+      y = 0;
+    }
+
+    cout << y <<endl;
+  }
+
   return 0;
 }
